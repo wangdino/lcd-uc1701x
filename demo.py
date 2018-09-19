ab9d4c27549596a1ffc5040834445d600d8f6dc0
wangdino <anguste@gmail.com>
2018-09-19 17:40:35 -0400

    Add backlight max in demo

--- demo.py
+++ demo.py
@@ -13,6 +13,7 @@ def main():
     # fill and clear LCD test
     lcd.fillDisplay()
     lcd.clearLCD()
+    lcd.fullBackLight()
 
     # display UTC time
     image = Image.new('1', (uc1701x.LCD_WIDTH, uc1701x.LCD_HEIGHT), 0)
