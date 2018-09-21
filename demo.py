81f2e08c362402c17acff0acae8567a7a625d294
wangdino <anguste@gmail.com>
2018-09-20 20:58:31 -0400

    New font test function

--- demo.py
+++ demo.py
@@ -20,9 +20,9 @@ def main():
     RobotoMono = ImageFont.truetype('fonts/RobotoMono-Regular.ttf', font_size)
     
     line1 = 'The quick brown fox jumps over the lazy dog.'
-    line2 = '0123456789'
-    line3 = 'Another new line.'
-    line4 = 'Yet another new line.'
+    line2 = '1234567890123456789'
+    line3 = 'PoiretOne'
+    line4 = 'Comfortaa'
 
     lcd = uc1701x.LCD()
     lcd.initialLCD()
@@ -36,14 +36,15 @@ def main():
     lcd.displayImage(image)
     lcd.delay(1000)
     draw = ImageDraw.Draw(image)
-    draw.text((0,0), line1, font = RobotoMono, spacing = 0, fill = 255)
+    draw.text((0,0), line1, font = UbuntuMono, spacing = 0, fill = 255)
     draw.text((0,17), line2, font = RobotoMono, spacing = 0, fill = 255)
-    draw.text((0,33), line3, font = RobotoMono, spacing = 0, fill = 255)
-    draw.text((0,49), line4, font = RobotoMono, spacing = 0, fill = 255)
+    draw.text((0,33), line3, font = PoiretOne, spacing = 0, fill = 255)
+    draw.text((0,49), line4, font = Comfortaa, spacing = 0, fill = 255)
     lcd.setFrameBuffer(image)
     lcd.displayImage(image)
     lcd.delay(60000)
     lcd.clearLCD()
+    lcd.offBackLight()
 
     # keep refreshing
 #    while True:
