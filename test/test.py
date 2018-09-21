74914dd9d1272d64f736861e834db091a5a56ed9
wangdino <anguste@gmail.com>
2018-09-21 16:32:28 -0400

    Update test function

--- test/test.py
+++ test/test.py
@@ -17,16 +17,18 @@ font_size_xl = 32 # Height about 40px
 Wenq = ImageFont.truetype('../fonts/wqy-zenhei.ttc', font_size_reg)
 NotoSansSC = ImageFont.truetype('../fonts/NotoSansSC-Regular.otf', font_size_reg)
 NotoSerifSC = ImageFont.truetype('../fonts/NotoSerifSC-Regular.otf', font_size_xl)
-#unifont = ImageFont.load('../fonts/unifont-11.0.02.pcf')
-Wenq9 = ImageFont.load('../fonts/unifont-11.0.02.bdf')
+unifont = ImageFont.load('../fonts/unifont-11.0.02.pil')
+Wenq9 = ImageFont.load('../fonts/wenquanyi_9pt.pil')
 
 line_SC = '这是坠吼的！'
 line_SC_XL = '吼哇！'
-line_EN = '1234567890123456789'
+line_num = '123456789012345678901234567890'
 
 #print('Wenq: ', Wenq.getsize(line_SC))
 #print('NotoSansSC: ', NotoSansSC.getsize(line_SC))
 #print('NotoSerifSC: ', NotoSerifSC.getsize(line_SC_XL))
+print(Wenq9.getsize(line_num))
+print(unifont.getsize(line_num))
 
 test_image = Image.new('1', (128, 64), 0)
 draw = ImageDraw.Draw(test_image)
@@ -34,7 +36,16 @@ draw = ImageDraw.Draw(test_image)
 #draw.text((0, 20), line_SC, font = NotoSansSC, spacing = 0, fill = 255)
 #draw.text((0, 41), line_SC, font = NotoSerifSC, spacing = 0, fill = 255)
 #draw.text((16, 12), line_SC_XL, font = NotoSerifSC, spacing = 0, fill = 255)
-draw.text((0,0), line_EN, font = Wenq9)
+
+#draw.text((0,0), line_num, font = unifont, fill = 255)
+#draw.text((0,17), line_num, font = unifont, fill = 255)
+#draw.text((0,33), line_num, font = unifont, fill = 255)
+#draw.text((0,49), line_num, font = unifont, fill = 255)
+
+draw.text((0,0), line_num, font = Wenq9, fill = 255)
+draw.text((0,17), line_num, font = Wenq9, fill = 255)
+draw.text((0,33), line_num, font = Wenq9, fill = 255)
+draw.text((0,49), line_num, font = Wenq9, fill = 255)
 
 
 imshow(test_image)
